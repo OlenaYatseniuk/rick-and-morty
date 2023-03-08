2ae10bc37425cfc6966d9ed9267e6966aa41f4a6
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-03-08 15:16:44 +0200

    add styles for mobile

--- src/modules/CharacterItem/CharacterItem.jsx
+++ src/modules/CharacterItem/CharacterItem.jsx
@@ -11,35 +11,35 @@ const CharacterItem = ({
       </div>
       <div className={s.character__titleInfo}>
         <p className={s.character__name}>{name}</p>
-        <p className={s.character__title}>Informations</p>
       </div>
+        <p className={s.character__title}>Informations</p>
       <ul className={s.character__info}>
         <li className={s.character__infoItem}>
-          <div className={s.example}>
+          <div className={s.character__infoItemWrapper}>
             <p className={s.character__infoTitleName}>Gender</p>
             <p className={s.character__infoBio}>{gender}</p>
           </div>
         </li>
         <li className={s.character__infoItem}>
-          <div className={s.example}>
+          <div className={s.character__infoItemWrapper}>
             <p className={s.character__infoTitleName}>Status</p>
             <p className={s.character__infoBio}>{status}</p>
           </div>
         </li>
         <li className={s.character__infoItem}>
-          <div className={s.example}>
+          <div className={s.character__infoItemWrapper}>
             <p className={s.character__infoTitleName}>Specie</p>
             <p className={s.character__infoBio}>{species}</p>
           </div>
         </li>
         <li className={s.character__infoItem}>
-          <div className={s.example}>
+          <div className={s.character__infoItemWrapper}>
             <p className={s.character__infoTitleName}>Origin</p>
             <p className={s.character__infoBio}>{origin?.name}</p>
           </div>
         </li>
         <li className={s.character__infoItem}>
-          <div className={s.example}>
+          <div className={s.character__infoItemWrapper}>
             <p className={s.character__infoTitleName}>Type</p>
             <p className={s.character__infoBio}>{!type ? 'Unknown' : type}</p>
           </div>
